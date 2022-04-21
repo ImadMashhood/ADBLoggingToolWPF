bde399f113950d6f0de19452d2fa9e219136b496
Imad Mashhood <mashhoodimad@gmail.com>
2022-04-21 16:14:09 -0500

    Add project files.

---
+++ ADBLoggingTool/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace ADBLoggingTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ipAddressTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fileNameTB = new System.Windows.Forms.TextBox();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.Status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ipAddressTB
+            // 
+            this.ipAddressTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ipAddressTB.Location = new System.Drawing.Point(120, 22);
+            this.ipAddressTB.Name = "ipAddressTB";
+            this.ipAddressTB.Size = new System.Drawing.Size(264, 26);
+            this.ipAddressTB.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(25, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP Address:";
+            // 
+            // startBtn
+            // 
+            this.startBtn.BackColor = System.Drawing.Color.White;
+            this.startBtn.FlatAppearance.BorderSize = 0;
+            this.startBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startBtn.Location = new System.Drawing.Point(407, 24);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 6;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = false;
+            this.startBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(25, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "File Name:";
+            // 
+            // fileNameTB
+            // 
+            this.fileNameTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileNameTB.Location = new System.Drawing.Point(120, 68);
+            this.fileNameTB.Name = "fileNameTB";
+            this.fileNameTB.Size = new System.Drawing.Size(264, 26);
+            this.fileNameTB.TabIndex = 5;
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.BackColor = System.Drawing.Color.White;
+            this.stopBtn.Enabled = false;
+            this.stopBtn.FlatAppearance.BorderSize = 0;
+            this.stopBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.stopBtn.Location = new System.Drawing.Point(407, 68);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(75, 23);
+            this.stopBtn.TabIndex = 6;
+            this.stopBtn.Text = "Stop";
+            this.stopBtn.UseVisualStyleBackColor = false;
+            this.stopBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Status
+            // 
+            this.Status.AutoSize = true;
+            this.Status.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Status.ForeColor = System.Drawing.Color.White;
+            this.Status.Location = new System.Drawing.Point(223, 114);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(0, 20);
+            this.Status.TabIndex = 7;
+            this.Status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(522, 166);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.fileNameTB);
+            this.Controls.Add(this.ipAddressTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ADB Logging Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox ipAddressTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox fileNameTB;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.Label Status;
+    }
+}
+
---
+++ ADBLoggingTool/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADBLoggingTool
+{
+    public partial class Form1 : Form
+    {
+        Process p = new Process();
+        bool endThread = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string run_process(string commands)
+        {
+            
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.Arguments = "/c " + commands;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.Start();
+            do
+            {
+                Application.DoEvents();
+            } while (!p.HasExited); 
+            return p.StandardOutput.ReadToEnd();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String ipAddress = ipAddressTB.Text;
+            String fileName = fileNameTB.Text;
+            if (!validateIPAddress(ipAddress))
+            {
+                MessageBox.Show("Invalid IP Address, please try again.");
+                return;
+            }
+            if (fileName.IndexOfAny(System.IO.Path.GetInvalidFileNameChars()) != -1)
+            {
+                MessageBox.Show("Invalid file name, please try again.");
+                return;
+            }
+            startBtn.Enabled = false;
+            stopBtn.Enabled = true;
+            Status.Text = "Logging IP: " + ipAddress;
+            new Thread(() =>
+            {
+                if (endThread)
+                {
+                    return;
+                }
+                Console.WriteLine(ipAddress);
+                Console.WriteLine(run_process("adb start-server"));
+                Console.WriteLine(run_process("adb connect"));
+                Console.WriteLine(run_process("adb logcat"));
+            }).Start();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            endThread = true;
+            Process stop = new Process();
+            stop.StartInfo.FileName = "cmd.exe";
+            stop.StartInfo.Arguments = "/c " + "adb kill-server";
+            stop.StartInfo.CreateNoWindow = true;
+            stop.StartInfo.UseShellExecute = false;
+            stop.StartInfo.RedirectStandardOutput = true;
+            stop.Start();
+            p.Kill();
+            Status.Text = "Logging complete, file saved";
+            endThread = false;
+            stopBtn.Enabled = false;
+            startBtn.Enabled = true;
+        }
+
+        private bool validateIPAddress(String ipAddress)
+        {
+            if (String.IsNullOrEmpty(ipAddress))
+                return false;
+
+            var items = ipAddress.Split('.');
+
+            if (items.Length != 4)
+                return false;
+            return items.All(item => byte.TryParse(item, out _));
+        }
+    }
+}
